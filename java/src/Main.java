7a6887ce67ccd1b4ed30c2bcc2fa0739f730644f
hyunmin <choihm9903@naver.com>
2022-03-02 19:37:32 +0900

    baekjoon: 1929

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int M = sc.nextInt();
 		int N = sc.nextInt();
 
-		for (int i=2; i<=N; i++) {
-			while (N%i==0) {
+		for(int i=M; i<=N; i++) {
+			if (isPrime(i)) {
 				System.out.println(i);
-				N /= i;
 			}
 		}
+	}
+
+	public static boolean isPrime(int n) {
+		if (n < 2)
+			return false;
+		for (int i = 2; i<=(int)Math.sqrt(n); i++)
+			if (n % i == 0)
+				return false;
+		return true;
 	}
 }
