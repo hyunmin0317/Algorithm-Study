4dfabd9690d0439ffd7831d7b1e169522b210c09
hyunmin <choihm9903@naver.com>
2022-03-02 19:07:43 +0900

    baekjoon: 1978

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,38 +1,25 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-	static void sort(String[] arr, int N) {
-		String temp;
-		for(int i = 0; i < N - 1; i++) {
-			for(int j= 1 ; j < N-i; j++) {
-				String str1 = arr[j - 1] + arr[j];
-				String str2 = arr[j] + arr[j - 1];
-				if(str1.compareTo(str2) < 0) {
-					temp = arr[j-1];
-					arr[j-1] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
-	}
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();;
-		String[] array = new String[N];
-		String answer = "";
-		for(int i=0;i<N;i++) {
-			array[i]=sc.next();
+		int N = sc.nextInt();
+		int i, num, cnt = 0;
+
+		for (i=0; i<N; i++) {
+			num = sc.nextInt();
+			if (isPrime(num))
+				cnt++;
 		}
+		System.out.println(cnt);
+	}
 
-		sort(array, N);
-		for (String num:array)
-			answer += num;
-
-		StringBuffer sb = new StringBuffer(answer);
-		if (sb.charAt(0) == '0')
-			System.out.println('0');
-		else
-			System.out.println(answer);
+	public static boolean isPrime(int n) {
+		if (n < 2)
+			return false;
+		for (int i = 2; i<=(int)Math.sqrt(n); i++)
+			if (n % i == 0)
+				return false;
+		return true;
 	}
 }
