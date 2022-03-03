dd2511df80a8129bb4410e053b19134fc82471b4
hyunmin <choihm9903@naver.com>
2022-03-03 22:16:42 +0900

    baekjoon: 1074

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,56 +1,38 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.Math.*;
 
 public class Main {
-	static int M, N, K;
-	static int[][] cabbage;
-	static boolean[][] visit;
-	static int count;
-	static int[] dx = {0, -1, 0, 1};
-	static int[] dy = {1, 0, -1, 0};
-
-	public static void dfs(int x, int y) {
-		visit[x][y] = true;
-
-		for (int i = 0; i < 4; i++) {
-			int cx = x + dx[i];
-			int cy = y + dy[i];
-
-			if (cx >= 0 && cy >= 0 && cx < M && cy < N)
-				if (!visit[cx][cy] && cabbage[cx][cy] == 1)
-					dfs(cx, cy);
-		}
-	}
+	static int N, r, c;
+	static int count=0;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+		N = (int) pow(2, sc.nextInt());
+		r = sc.nextInt();
+		c = sc.nextInt();
+		find(N, r, c);
+		System.out.println(count);
+	}
 
-		for (int i=0; i<T; i++) {
-			count = 0;
-			M = sc.nextInt();
-			N = sc.nextInt();
-			K = sc.nextInt();
-			cabbage = new int[M][N];
-			visit = new boolean[M][N];
+	private static void find(int size, int r, int c) {
+		if(size == 1)
+			return;
 
-			for (int j=0; j<K; j++) {
-				int X = sc.nextInt();
-				int Y = sc.nextInt();
-				cabbage[X][Y] = 1;
-			}
-
-			for (int x = 0; x < M; x++) {
-				for (int y = 0; y < N; y++) {
-					if (cabbage[x][y] == 1 && !visit[x][y]) {
-						dfs(x, y);
-						count++;
-					}
-				}
-			}
-			System.out.println(count);
+		if(r < size/2 && c < size/2) {
+			find(size/2, r, c);
+		}
+		else if(r < size/2 && c >= size/2) {
+			count += size * size / 4;
+			find(size/2, r, c - size/2);
+		}
+		else if(r >= size/2 && c < size/2) {
+			count += (size * size / 4) * 2;
+			find(size/2, r - size/2, c);
+		}
+		else {
+			count += (size * size / 4) * 3;
+			find(size/2, r - size/2, c - size/2);
 		}
 	}
 }
