b0575e0a775ffa80a88c19fa3a08535a79f74dd9
hyunmin <choihm9903@naver.com>
2022-03-06 21:58:26 +0900

    baekjoon: 1927

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,22 +1,26 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int N, x;
-		Scanner sc = new Scanner(System.in);
-		N=sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> queue = new PriorityQueue();
+		N=Integer.parseInt(br.readLine());
 
 		for (int i=0; i<N; i++) {
-			x = sc.nextInt();
+			x = Integer.parseInt(br.readLine());
 
 			if (x==0) {
-
+				if (queue.isEmpty())
+					System.out.println(0);
+				else
+					System.out.println(queue.poll());
 			}
-			else {
-
-			}
+			else
+				queue.add(x);
 		}
-
-
 	}
 }
