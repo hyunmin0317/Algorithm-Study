3081f38e55fdcf6a7ffefcdd91e3e69e98000ac6
hyunmin <choihm9903@naver.com>
2022-03-07 00:41:30 +0900

    baekjoon: 1992

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,27 +1,48 @@
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int num, cnt=0;
-		Number[] numbers = new Number[N];
-		for (int i=0;i<N;i++) {
-			numbers[i] = new Number();
-			numbers[i].x = sc.nextInt();
-			numbers[i].y = sc.nextInt();
-		}
-		Arrays.sort(numbers);
 
-		num=numbers[0].y;
-		cnt++;
+	public static boolean confirm(int x, int y, int size) {
+		int n = list[x][y];
+		for (int i=x; i<x+size; i++)
+			for (int j=y; j<y+size; j++)
+				if (n!=list[i][j])
+					return false;
+		return true;
+	}
 
-		for (int i=1;i<N;i++) {
-			if (numbers[i].x>=num) {
-				num=numbers[i].y;
-				cnt++;
+	public static void count(int x, int y, int size) {
+		if (size==1)
+			System.out.print(list[x][y]);
+		else {
+			if (confirm(x, y, size)) {
+				System.out.print(list[x][y]);
+			}
+			else {
+				System.out.print("(");
+				for (int i=x; i<x+size; i+=size/2)
+					for (int j=y; j<y+size; j+=size/2)
+						count(i, j, size/2);
+				System.out.print(")");
 			}
 		}
-		System.out.println(cnt);
+	}
+
+	public static int[][] list;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] numbers;
+		int i, j;
+		int N=sc.nextInt();
+		list = new int[N][N];
+
+		for (i=0; i<N; i++) {
+			numbers=sc.next().split("");
+			for (j=0; j<N; j++)
+				list[i][j]=Integer.parseInt(numbers[j]);
+		}
+
+		count(0,0,N);
 	}
 }
