7372b325c6c693672c0d9c85866420dac796c2fe
hyunmin <choihm9903@naver.com>
2022-03-06 15:28:54 +0900

    baekjoon: 1463

---
+++ java/src/Baekjoon1463.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+import static java.lang.Math.min;
+
+public class Baekjoon1463 {
+    static int N, num;
+    static int MAX=1000001;
+    static int[] cal= new int[MAX];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        cal[1] = 0;
+        for (int i=2;i<=N;i++) {
+            num=cal[i-1];
+            if (i%2==0)
+                num = min(num,cal[i/2]);
+            if (i%3==0)
+                num = min(num,cal[i/3]);
+            cal[i] = num+1;
+        }
+        System.out.println(cal[N]);
+    }
+}
--- java/src/Main.java
+++ java/src/Main.java
@@ -1,66 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
+import static java.lang.Math.min;
 
 public class Main {
-	static Queue<Integer> queue;
-	static boolean[][] vertex;
-	static boolean[] visit;
-	static int[] values;
-	static int MAX = 101;
-	static int N, M;
-
-	public static int bfs(int i) {
-		int sum=0;
-		visit[i] = true;
-		queue.offer(i);
-
-		while(!queue.isEmpty()) {
-			int temp = queue.poll();
-			for(int j = 1; j <= N; j++) {
-				if(vertex[temp][j] && !visit[j]) {
-					visit[j] = true;
-					queue.offer(j);
-					values[j] = values[temp]+1;
-				}
-			}
-		}
-
-		for (var value:values)
-			sum += value;
-		return sum;
-	}
+	static int N, num;
+	static int MAX=1000001;
+	static int[] cal= new int[MAX];
 
 	public static void main(String[] args) {
-		int min=Integer.MAX_VALUE;
-		int num, ans=0;
 		Scanner sc = new Scanner(System.in);
-		List<Integer> numbers = new ArrayList();
-		queue = new LinkedList();
-		vertex = new boolean[MAX][MAX];
-
 		N = sc.nextInt();
-		M = sc.nextInt();
 
-		for (int i=0; i<M; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-
-			if (!numbers.contains(x))
-				numbers.add(x);
-			if (!numbers.contains(y))
-				numbers.add(y);
-			vertex[x][y] = vertex[y][x] = true;
+		cal[1] = 0;
+		for (int i=2;i<=N;i++) {
+			num=cal[i-1];
+			if (i%2==0)
+				num = min(num,cal[i/2]);
+			if (i%3==0)
+				num = min(num,cal[i/3]);
+			cal[i] = num+1;
 		}
-		Collections.sort(numbers);
-
-		for (var number:numbers) {
-			visit = new boolean[MAX];
-			values = new int[MAX];
-			num = bfs(number);
-			if (min>num) {
-				min = num;
-				ans = number;
-			}
-		}
-		System.out.println(ans);
+		System.out.println(cal[N]);
 	}
 }
