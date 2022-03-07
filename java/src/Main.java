dbb61733fa9aba218bf704ed10609aa25d9892ac
hyunmin <choihm9903@naver.com>
2022-03-08 00:05:00 +0900

    baekjoon: 11279

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,44 +3,22 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		int N, x;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		PriorityQueue<Integer> queue = new PriorityQueue(Collections.reverseOrder());
+		N=Integer.parseInt(br.readLine());
 
-		List<Integer> list = new ArrayList();
-		int M=Integer.parseInt(br.readLine());
-		String op;
-		int num;
+		for (int i=0; i<N; i++) {
+			x = Integer.parseInt(br.readLine());
 
-		for (int i=0; i<M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			op = st.nextToken();
-
-			if (op.equals("empty")) {
-				list.clear();
-			} else if (op.equals("all")) {
-				list.clear();
-				for (int j=1; j<=20; j++)
-					list.add(j);
-			} else {
-				num = Integer.parseInt(st.nextToken());
-				if (op.equals("add")) {
-					if (!list.contains(num))
-						list.add(num);
-				} else if (op.equals("remove")) {
-					list.remove(Integer.valueOf(num));
-				} else if (op.equals("toggle")) {
-					if (list.contains(num))
-						list.remove(Integer.valueOf(num));
-					else
-						list.add(num);
-				} else {
-					if (list.contains(num))
-						bw.write(String.valueOf(1)+"\n");
-					else
-						bw.write(String.valueOf(0)+"\n");
-				}
+			if (x==0) {
+				if (queue.isEmpty())
+					System.out.println(0);
+				else
+					System.out.println(queue.poll());
 			}
+			else
+				queue.add(x);
 		}
-		bw.flush();
 	}
 }
