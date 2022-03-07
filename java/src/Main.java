917ba8d8834f0aee73a61e87789463f41a76e39a
hyunmin <choihm9903@naver.com>
2022-03-07 23:59:10 +0900

    baekjoon: 11723

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,20 +1,46 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		int T, n=11;
-		int[] cnt = new int[n];
-		Scanner sc = new Scanner(System.in);
-		T=sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		cnt[1]=1;
-		cnt[2]=2;
-		cnt[3]=4;
+		List<Integer> list = new ArrayList();
+		int M=Integer.parseInt(br.readLine());
+		String op;
+		int num;
 
-		for (int i=4; i<n; i++)
-			cnt[i] = cnt[i-1] + cnt[i-2] + cnt[i-3];
+		for (int i=0; i<M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			op = st.nextToken();
 
-		for (int i=0; i<T; i++)
-			System.out.println(cnt[sc.nextInt()]);
+			if (op.equals("empty")) {
+				list.clear();
+			} else if (op.equals("all")) {
+				list.clear();
+				for (int j=1; j<=20; j++)
+					list.add(j);
+			} else {
+				num = Integer.parseInt(st.nextToken());
+				if (op.equals("add")) {
+					if (!list.contains(num))
+						list.add(num);
+				} else if (op.equals("remove")) {
+					list.remove(Integer.valueOf(num));
+				} else if (op.equals("toggle")) {
+					if (list.contains(num))
+						list.remove(Integer.valueOf(num));
+					else
+						list.add(num);
+				} else {
+					if (list.contains(num))
+						bw.write(String.valueOf(1)+"\n");
+					else
+						bw.write(String.valueOf(0)+"\n");
+				}
+			}
+		}
+		bw.flush();
 	}
 }
