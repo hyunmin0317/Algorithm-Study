7d8461df7e64d5176b2c0bb04b7dde441720e5e0
hyunmin <choihm9903@naver.com>
2022-03-08 09:33:21 +0900

    baekjoon: 11726

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,40 +1,15 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-	public static int N, M;
-	public static boolean[][] vertex;
-	public static boolean[] visit;
-
-	public static void dfs(int n) {
-		visit[n] = true;
-
-		for (int i = 1; i <= N; i++) {
-			if (!visit[i] && vertex[n][i])
-				dfs(i);
-		}
-	}
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int x, y;
-		int count=0;
-		N = sc.nextInt();
-		M = sc.nextInt();
-		vertex = new boolean[N+1][N+1];
-		visit = new boolean[N+1];
+		int n = sc.nextInt();
+		int cnt[] = new int[n+1];
 
-		for (int i=0; i<M; i++) {
-			x = sc.nextInt();
-			y = sc.nextInt();
-			vertex[x][y]=vertex[y][x]=true;
-		}
-
-		for (x = 1; x <= N; x++) {
-			if (!visit[x]) {
-				dfs(x);
-				count++;
-			}
-		}
-		System.out.println(count);
+		cnt[1] = 1;
+		cnt[2] = 2;
+		for (int i=3; i<=n; i++)
+			cnt[i] = (cnt[i-1]+cnt[i-2])%10007;
+		System.out.println(cnt[n]);
 	}
 }
