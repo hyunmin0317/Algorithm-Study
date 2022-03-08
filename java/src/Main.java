719c87bb8a98d1a4587c156b06328a628008b1e7
hyunmin <choihm9903@naver.com>
2022-03-08 18:50:05 +0900

    baekjoon: 7576

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,48 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	static Queue<int[]> queue;
+	static int M ,N;
+	static int[][] vertex;
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
+
+	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T, k, n;
-		String op, input[];
-		TreeMap<Integer, Integer> treeMap;
-		T = Integer.parseInt(br.readLine());
+		String[] str = br.readLine().split(" ");
+		M = Integer.parseInt(str[0]);
+		N = Integer.parseInt(str[1]);
+		vertex = new int[N][M];
+		queue = new LinkedList();
 
-		for (int i=0; i<T; i++) {
-			treeMap = new TreeMap();
-			k = Integer.parseInt(br.readLine());
-			for (int j=0; j<k; j++) {
-				input = br.readLine().split(" ");
-				op=input[0];
-				n=Integer.parseInt(input[1]);
-
-				if (op.equals("I"))
-					treeMap.put(n, treeMap.getOrDefault(n, 0) + 1);
-				else {
-					if(!treeMap.isEmpty())
-						if(n == -1) {
-							int minKey = treeMap.firstKey();
-							if(treeMap.get(minKey) == 1)
-								treeMap.remove(minKey);
-							else
-								treeMap.put(minKey, treeMap.get(minKey) - 1);
-						}
-						else {
-							int maxKey = treeMap.lastKey();
-							if(treeMap.get(maxKey) == 1)
-								treeMap.remove(maxKey);
-							else
-								treeMap.put(maxKey, treeMap.get(maxKey) - 1);
-						}
-				}
+		for (int i = 0; i < N; i++) {
+			str = br.readLine().split(" ");
+			for (int j = 0; j < M; j++) {
+				vertex[i][j] = Integer.parseInt(str[j]);
+				if (vertex[i][j] == 1)
+					queue.add(new int[]{i, j});
 			}
-
-			if(treeMap.isEmpty())
-				bw.write("EMPTY\n");
-			else
-				bw.write(treeMap.lastKey() + " " + treeMap.firstKey() + "\n");
 		}
-		bw.flush();
+		bfs();
+		System.out.println(ans());
+	}
+
+	public static int ans() {
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (vertex[i][j] == 0) {
+					return -1;
+				}
+				max = Math.max(max, vertex[i][j]);
+			}
+		}
+		return max-1;
+	}
+
+	public static void bfs() {
+		while (!queue.isEmpty()) {
+			int[] num = queue.poll();
+			for (int i = 0; i < 4; i++) {
+				int nextX = num[0] + dx[i];
+				int nextY = num[1] + dy[i];
+				if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M || vertex[nextX][nextY] != 0)
+					continue;
+				vertex[nextX][nextY] = vertex[num[0]][num[1]] + 1;
+				queue.add(new int[]{nextX, nextY});
+			}
+		}
 	}
 }
