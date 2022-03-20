b9b69e804684756028c555090d7d0707c62850d9
hyunmin <choihm9903@naver.com>
2022-03-20 18:56:00 +0900

    baekjoon:5430

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,50 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-	static int[][] vertex;
-	static int N;
-
-	public static void bfs(int i) {
-		Queue<Integer> queue = new LinkedList();
-		int[] visit = new int[N];
-		queue.offer(i);
-
-		while(!queue.isEmpty()) {
-			int temp = queue.poll();
-			for(int j = 0; j < N; j++) {
-				if(vertex[temp][j]==1 && visit[j]==0) {
-					visit[j] = 1;
-					queue.offer(j);
-				}
-			}
-		}
-		for (int n:visit)
-			System.out.print(n+" ");
-		System.out.println();
-	}
+	static Deque<Integer> deque;
+	static String commands;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		vertex = new int[N][N];
+		int n, T = sc.nextInt();
 
-		for (int i=0; i<N; i++)
-			for (int j=0; j<N; j++)
-				vertex[i][j] = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			commands = sc.next();
+			n = sc.nextInt();
+			String arr = sc.next();
+			deque = new LinkedList<>();
+			for (String s : arr.substring(1, arr.length() - 1).split(","))
+				if (!s.equals(""))
+					deque.add(Integer.valueOf(s));
 
-		for (int i=0; i<N; i++)
-			bfs(i);
+			System.out.println(ac());
+		}
+	}
+
+	public static String ac() {
+		boolean reverse = false;
+
+		for (char command : commands.toCharArray()) {
+			if (command == 'R')
+				reverse = !reverse;
+			else {
+				if (deque.size() == 0)
+					return "error";
+				if (reverse)
+					deque.removeLast();
+				else
+					deque.removeFirst();
+			}
+		}
+
+		StringBuilder sb = new StringBuilder("[");
+		while (!deque.isEmpty()) {
+			sb.append(reverse ? deque.removeLast() : deque.removeFirst());
+			if (deque.size() != 0)
+				sb.append(',');
+		}
+		sb.append(']');
+
+		return sb.toString();
 	}
 }
