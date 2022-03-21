123c6a3b5eb301bccf35f7b026aba91a20fe7811
hyunmin <choihm9903@naver.com>
2022-03-21 13:28:07 +0900

    baekjoon: 9019

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,58 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	static final int MAX = 101;
-	static boolean[] visit = new boolean[MAX];
-	static int[] list = new int[MAX];
-	static int[] arr = new int[MAX];
+	static final int MAX = 10000;
+	static Queue<Integer> queue = new LinkedList();
+	static String[] ans;
+	static boolean[] visit;
 
 	public static void bfs(int x) {
-		Queue<Integer> queue = new LinkedList();
+		int D, S, L, R;
+		visit = new boolean[MAX];
+		ans = new String[MAX];
+		for (int i=0; i<MAX; i++)
+			ans[i] = "";
+
 		visit[x] = true;
 		queue.add(x);
 
 		while(!queue.isEmpty()) {
 			int temp = queue.poll();
-			for (int i = 1; i <= 6; i++) {
-				int go = temp+i;
 
-				if (go >= 0 && go <= 100) {
-					if (list[go]!=0)
-						go = list[go];
-					if (!visit[go]) {
-						queue.add(go);
-						arr[go] = arr[temp] + 1;
-						visit[go] = true;
-					}
-				}
+			D = (temp*2) % 10000;
+			S = (temp==0)?9999:temp-1;
+			L = (temp%1000)*10+temp/1000;
+			R = (temp%10)*1000+temp/10;
+
+			if (!visit[D]) {
+				queue.add(D);
+				ans[D] = ans[temp] + "D";
+				visit[D] = true;
+			}
+			if (!visit[S]) {
+				queue.add(S);
+				ans[S] = ans[temp] + "S";
+				visit[S] = true;
+			}
+			if (!visit[L]) {
+				queue.add(L);
+				ans[L] = ans[temp] + "L";
+				visit[L] = true;
+			}
+			if (!visit[R]) {
+				queue.add(R);
+				ans[R] = ans[temp] + "R";
+				visit[R] = true;
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		int N, M;
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		for (int i=0; i<N+M; i++)
-			list[sc.nextInt()] = sc.nextInt();
-
-		bfs(1);
-		System.out.println(arr[100]);
+		int T = sc.nextInt();
+		for (int i=0; i<T; i++) {
+			bfs(sc.nextInt());
+			System.out.println(ans[sc.nextInt()]);
+		}
 	}
 }
