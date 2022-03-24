5fbe9ae48ed5feca9a14ba739185750c82f883fa
hyunmin <choihm9903@naver.com>
2022-03-24 15:03:02 +0900

    baekjoon: 12852

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
-public class Main {
-	public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
+	static int N;
+	static int[] arr, path;
+
+	public static void bfs() {
+		for (int i = 2; i <= N; i++) {
+			if(i%3 == 0) {
+				if(arr[i/3] +1 < arr[i]) {
+					arr[i] = arr[i/3] +1;
+					path[i] = i/3;
+				}
+			}
+
+			if(i%2 == 0) {
+				if(arr[i/2] +1 < arr[i]) {
+					arr[i] = arr[i/2] +1;
+					path[i] = i/2;
+				}
+			}
+
+			if(arr[i-1] +1 < arr[i]) {
+				arr[i] = arr[i-1] +1;
+				path[i] = i-1;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 N = sc.nextInt();
+		arr = new int[N+1];
+		path = new int[N+1];
+		Arrays.fill(arr, Integer.MAX_VALUE);
+		arr[1] = 0;
+		bfs();
+
+		System.out.println(arr[N]);
+		while (N > 0) {
+			System.out.print(N+" ");
+			N = path[N];
+		}
 	}
 }
