afd11170d7ed1cc16147c0601a31c4adb9ea8021
hyunmin <choihm9903@naver.com>
2022-03-24 16:10:55 +0900

    baekjoon: 15650

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,34 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int N;
-	static int[] arr, path;
-
-	public static void bfs() {
-		for (int i = 2; i <= N; i++) {
-			if(i%3 == 0) {
-				if(arr[i/3] +1 < arr[i]) {
-					arr[i] = arr[i/3] +1;
-					path[i] = i/3;
-				}
-			}
-
-			if(i%2 == 0) {
-				if(arr[i/2] +1 < arr[i]) {
-					arr[i] = arr[i/2] +1;
-					path[i] = i/2;
-				}
-			}
-
-			if(arr[i-1] +1 < arr[i]) {
-				arr[i] = arr[i-1] +1;
-				path[i] = i-1;
-			}
-		}
-	}
+	static int M, N;
+	static boolean[] visit;
+	static int[] arr;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		 N = sc.nextInt();
-		arr = new int[N+1];
-		path = new int[N+1];
-		Arrays.fill(arr, Integer.MAX_VALUE);
-		arr[1] = 0;
-		bfs();
+		N = sc.nextInt();
+		M = sc.nextInt();
+		visit = new boolean[N+1];
+		arr = new int[M];
+		dfs(0);
+	}
 
-		System.out.println(arr[N]);
-		while (N > 0) {
-			System.out.print(N+" ");
-			N = path[N];
+	private static void dfs(int index) {
+		if (index == M) {
+			for (int num: arr)
+				System.out.print(num+" ");
+			System.out.println();
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			if(!visit[i]) {
+				arr[index] = i;
+				visit[i] = true;
+				dfs(index + 1);
+				for (int j = i + 1; j <= N; j++)
+					visit[j] = false;
+			}
 		}
 	}
 }
