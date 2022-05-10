0c3b0f2330a25e3ecbcd8f2482ddefe89a7d7cf8
hyunmin <choihm9903@naver.com>
2022-05-10 18:45:55 +0900

    baekjoon: 2908

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int hour, minute;
 		Scanner sc = new Scanner(System.in);
-		hour = sc.nextInt();
-		minute = sc.nextInt();
+		int n1, n2;
 
-		if (minute<45) {
-			minute += 15;
-			hour--;
-			if (hour==-1)
-				hour = 23;
-		} else
-			minute -= 45;
+		n1 = sc.nextInt();
+		n2 = sc.nextInt();
+		n1 = change(n1);
+		n2 = change(n2);
 
-		System.out.println(hour+" "+minute);
+		if (n1>n2)
+			System.out.println(n1);
+		else
+			System.out.println(n2);
+	}
+
+	public static int change(int num) {
+		int i, sum=0;
+		for (i=2; i>=0; i--) {
+			sum += (num % 10) * pow(10, i);
+			num /= 10;
+		}
+		return sum;
 	}
 }
