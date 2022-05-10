71f9367bc7337961251b98dff3f34417186d828b
hyunmin <choihm9903@naver.com>
2022-05-10 19:06:00 +0900

    baekjoon: 10809

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,21 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		int i, index, N=26;
 		Scanner sc = new Scanner(System.in);
-		int i, num, X, N = sc.nextInt();
-		X = sc.nextInt();
+		int[] alphabet = new int[N];
+		String word = sc.next();
 
-		for (i=0; i<N; i++) {
-			num = sc.nextInt();
-			if (num<X)
-				System.out.print(num+" ");
+		for (i=0; i<N; i++)
+			alphabet[i] = -1;
+
+		for (i=0; i<word.length(); i++) {
+			index = word.charAt(i)-'a';
+			if (alphabet[index]==-1)
+				alphabet[index] = i;
 		}
+
+		for (i=0; i<N; i++)
+			System.out.print(alphabet[i]+" ");
 	}
 }
