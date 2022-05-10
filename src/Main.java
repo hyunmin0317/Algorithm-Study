c0f6dc7910e840db526ed200eb97d8d7230ea234
hyunmin <choihm9903@naver.com>
2022-05-10 10:30:27 +0900

    baekjoon: 2475

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int i, N = sc.nextInt();
+		int i, n, sum=0;
 
-		for (i=1; i<=N; i++) {
-			System.out.print(" ".repeat(N-i));
-			System.out.println("*".repeat(i));
+		for (i=0; i<5; i++) {
+			n = sc.nextInt();
+			sum += n*n;
 		}
-
+		System.out.println(sum%10);
 	}
 }
