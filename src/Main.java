06b76406cfca034f4d62b8ef1a743e82d52c3a53
hyunmin <choihm9903@naver.com>
2022-05-10 10:26:37 +0900

    baekjoon: 2439

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,14 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-	static int R, C;
-	static int ans = 0;
-	static int[][] board;
-	static boolean[] visit = new boolean[26];
-	static int[] dx = {0, -1, 0, 1};
-	static int[] dy = {1, 0, -1, 0};
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i, N = sc.nextInt();
 
-	public static void dfs(int x, int y, int cnt) {
-		if (visit[board[x][y]]) {
-			ans = Math.max(ans, cnt);
-			return;
-		}
-		else {
-			visit[board[x][y]] = true;
-			for (int i = 0; i < 4; i++) {
-				int cx = x + dx[i];
-				int cy = y + dy[i];
-
-				if (cx >= 0 && cy >= 0 && cx < R && cy < C)
-					dfs(cx, cy, cnt+1);
-			}
-			visit[board[x][y]] = false;
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		board = new int[R][C];
-
-		for (int i=0; i<R; i++) {
-			String a = br.readLine();
-			for (int j=0; j<C; j++)
-				board[i][j] = a.charAt(j)-'A';
+		for (i=1; i<=N; i++) {
+			System.out.print(" ".repeat(N-i));
+			System.out.println("*".repeat(i));
 		}
 
-		dfs(0,0, 0);
-		System.out.println(ans);
 	}
 }
