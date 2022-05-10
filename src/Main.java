02e662b12a2dca03239ac033eeed3b7200df4dad
hyunmin <choihm9903@naver.com>
2022-05-10 18:28:35 +0900

    baekjoon: 2753

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int i, N = sc.nextInt();
+		int year = sc.nextInt();
 
-		for (i=N; i>0; i--) {
-			System.out.println(i);
-		}
+		if (year%400==0||(year%4==0&&year%100!=0))
+			System.out.println(1);
+		else
+			System.out.println(0);
 	}
 }
