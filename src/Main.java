3424a15342dfc1a64bb2c55c8f75c966d07e6aff
hyunmin <choihm9903@naver.com>
2022-05-10 10:34:23 +0900

    baekjoon: 2577

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,21 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int i, n, sum=0;
+		int i, n, sum=1;
+		int[] num = new int[10];
 
-		for (i=0; i<5; i++) {
+		for (i=0; i<3; i++) {
 			n = sc.nextInt();
-			sum += n*n;
+			sum *= n;
 		}
-		System.out.println(sum%10);
+
+		while (sum!=0) {
+			num[sum%10]++;
+			sum /= 10;
+		}
+
+		for (i=0; i<10; i++) {
+			System.out.println(num[i]);
+		}
 	}
 }
