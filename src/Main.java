d044419060fd2ce31e42faffba4989c554fc5b32
hyunmin <choihm9903@naver.com>
2022-05-10 18:51:48 +0900

    baekjoon: 3052

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,20 @@
 import java.util.Scanner;
 
-import static java.lang.Math.pow;
 
 public class Main {
 
 	public static void main(String[] args) {
+		int i, N=42, sum=0;
 		Scanner sc = new Scanner(System.in);
-		int n1, n2;
+		int[] list = new int[N];
 
-		n1 = sc.nextInt();
-		n2 = sc.nextInt();
-		n1 = change(n1);
-		n2 = change(n2);
+		for (i=0; i<10; i++)
+			list[sc.nextInt()%N]++;
 
-		if (n1>n2)
-			System.out.println(n1);
-		else
-			System.out.println(n2);
-	}
-
-	public static int change(int num) {
-		int i, sum=0;
-		for (i=2; i>=0; i--) {
-			sum += (num % 10) * pow(10, i);
-			num /= 10;
+		for (i=0; i<N; i++) {
+			if (list[i]!=0)
+				sum++;
 		}
-		return sum;
+		System.out.println(sum);
 	}
 }
