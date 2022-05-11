b104fff328cc41942fae7be1c5ad449f99a3c0f4
hyunmin <choihm9903@naver.com>
2022-05-11 10:05:43 +0900

    baekjoon: 2798

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		int i, j, k, N, M;
+		int sum, max=0;
 		Scanner sc = new Scanner(System.in);
-		int r = 31, M = 1234567891;
-		long sum = 0, pow = 1;
-		int i, L = sc.nextInt();
-		String str = sc.next();
+		N = sc.nextInt();
+		M = sc.nextInt();
+		int[] cards = new int[N];
 
-		for (i=0; i<L; i++) {
-			sum += (str.charAt(i)-'a'+1) * pow % M;
-			pow = pow * r % M;
-		}
-		System.out.println(sum % M);
+		for (i=0; i<N; i++)
+			cards[i] = sc.nextInt();
+
+		for (i=0; i<N; i++)
+			for (j=i+1; j<N; j++)
+				for (k=j+1; k<N; k++) {
+					sum = cards[i]+cards[j]+cards[k];
+					if (max < sum && sum <= M)
+						max = sum;
+				}
+		System.out.println(max);
 	}
 }
