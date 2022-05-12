42f6c786dabcfc777adb427d9fb59bb3be30fdab
hyunmin <choihm9903@naver.com>
2022-05-12 12:45:22 +0900

    baekjoon: 11050

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int tmp=0, num=1, ans=1;
-		int N = sc.nextInt();
+		int i, N, K, a=1, b=1;
+		N = sc.nextInt();
+		K = sc.nextInt();
 
-		while (true) {
-			if (tmp<=N&&N<=num)
-				break;
-			tmp = num+1;
-			num += 6*ans++;
-		}
-		System.out.println(ans);
+		for (i=0; i<K; i++)
+			a *= N--;
+		for (i=1; i<=K; i++)
+			b *= i;
+		System.out.println(a/b);
 	}
 }
