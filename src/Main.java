1761e2fee97cb9a92672ce631851a7ee56addac1
hyunmin <choihm9903@naver.com>
2022-05-12 13:28:07 +0900

    baekjoon: 2775

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String number;
+		int i, k, n, T = sc.nextInt();
 
-		while (true) {
-			number = sc.next();
-			if (number.equals("0"))
-				break;
-
-			if (check(number))
-				System.out.println("yes");
-			else
-				System.out.println("no");
+		for (i=0; i<T; i++) {
+			k = sc.nextInt();
+			n = sc.nextInt();
+			System.out.println(fun(k, n));
 		}
 	}
 
-	public static boolean check(String num) {
-		int i, len=num.length()-1;
-
-		for (i=0; i<=len; i++)
-			if (num.charAt(i)!=num.charAt(len-i))
-				return false;
-		return true;
+	public static int fun(int k, int n) {
+		if (k==0)
+			return n;
+		else if (n==1)
+			return 1;
+		else
+			return fun(k, n-1)+fun(k-1, n);
 	}
 }
