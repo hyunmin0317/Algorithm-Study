5a4b9e690da5c3a562c7def3f276c020db802dbc
hyunmin <choihm9903@naver.com>
2022-05-26 15:47:53 +0900

    Codility: lesson3-3

--- Codility/lesson3-3.py
+++ Codility/lesson3-3.py
@@ -1,22 +1,11 @@
-def solution1(A):
-    N = len(A)
-    min = abs(sum(A))
-
-    for i in range(1, N):
-        ans = abs(sum(A[:i]) - sum(A[i:]))
-        if min > ans:
-            min = ans
-    return min
-
 def solution(A):
-    N = len(A)
-    total = abs(sum(A))
-    min = total
+    total = sum(A)
+    a = A[0]
+    min = abs(2*a-total)
 
-    for i in range(1, N):
-        ans = abs(2*sum(A[:i]) - total)
+    for i in range(1, len(A)-1):
+        a += A[i]
+        ans = abs(2*a-total)
         if min > ans:
             min = ans
     return min
-
-print(solution([3,1,2,4,3]))
