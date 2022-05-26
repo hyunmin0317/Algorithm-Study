8ddf95859e330b69379e8b49ae2d9ae46dad983e
hyunmin <choihm9903@naver.com>
2022-05-26 14:27:29 +0900

    Codility: lesson3-2

--- Codility/lesson3-2.py
+++ Codility/lesson3-2.py
@@ -1,12 +1,10 @@
 def solution(A):
     N = len(A)
-    ans = [True for i in range(N+1)]
+    ans = [True for i in range(N+2)]
 
     for num in A:
         ans[num] = False
 
-    for i in range(0, N+1):
+    for i in range(1, N+2):
         if ans[i]:
             return i
-
-print(solution([2,3,1,5]))
