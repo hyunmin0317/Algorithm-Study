842121a38bd41cd2032c4267cd0aef3893018620
hyunmin <choihm9903@naver.com>
2022-05-27 14:16:36 +0900

    Codility: lesson7-3

---
+++ Codility/lesson7-3.py
@@ -0,0 +1,14 @@
+def solution(S):
+    dic = {')':'('}
+    stack = []
+    for s in S:
+        stack.append(s)
+
+        if s in dic.keys():
+            stack.pop()
+            if len(stack) == 0:
+                return 0
+            p = stack.pop()
+            if p != dic[s]:
+                return 0
+    return 1 if len(stack) == 0 else 0
