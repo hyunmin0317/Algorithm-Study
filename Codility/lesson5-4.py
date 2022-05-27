8fb89e1c9b982fc3b04f2cd8b9fab4de2d1ab491
hyunmin <choihm9903@naver.com>
2022-05-27 12:13:50 +0900

    Codility: lesson5-4

--- Codility/lesson5-4.py
+++ Codility/lesson5-4.py
@@ -1,16 +1,14 @@
-def avg(A):
-    return sum(A) / len(A)
-
-
 def solution(A):
-    N = len(A)
-    min = avg(A[0:2])
-    idx = -1
+    minAvg = (A[0] + A[1]) / 2
+    minIndex = 0
+    for i in range(2, len(A)):
+        avg = (A[i - 2] + A[i - 1] + A[i]) / 3
+        if (minAvg > avg):
+            minAvg = avg
+            minIndex = i - 2
 
-    for i in range(0, N - 2):
-        for j in range(i + 2, N - 1):
-            a = avg(A[i:j])
-            if min > a:
-                min = a
-                idx = i
-    return idx
+        avg = (A[i - 1] + A[i]) / 2
+        if (minAvg > avg):
+            minAvg = avg
+            minIndex = i - 1
+    return minIndex
