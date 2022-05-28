28b93cde836a45eb7199bd5ddabf39c039911361
hyunmin <choihm9903@naver.com>
2022-05-28 19:53:20 +0900

    Codility: lesson9-1

--- Codility/lesson9-1.py
+++ Codility/lesson9-1.py
@@ -1,8 +1,9 @@
 def solution(A):
     ans = 0
-    m = A[-1]
-    for i in range(len(A)-2, 0, -1):
-        n = max(m, A[i+1]) - A[i]
+    m = 0
+    for i in range(len(A)-2, -1, -1):
+        m = max(m, A[i+1])
+        n = m - A[i]
         if ans < n:
             ans = n
     return ans
