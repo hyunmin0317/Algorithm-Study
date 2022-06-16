9d600cba6d838608e22461b5d9ca69d7d04c7315
hyunmin <choihm9903@naver.com>
2022-06-16 18:26:46 +0900

    baekjoon: 11651

---
+++ Python/baekjoon11651.py
@@ -0,0 +1,10 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, sys.stdin.readline().split())))
+arr.sort(key=lambda x: (x[1], x[0]))
+
+for point in arr:
+    print(point[0], point[1])
