1a272fc3a7e7560c01bb6119c5bd61ae4b0ed9a6
hyunmin.0317 <hyunmin.0317@navercorp.com>
2022-11-28 13:40:06 +0900

    baekjoon: 1181

--- Python/baekjoon1181.py
+++ Python/baekjoon1181.py
@@ -1,25 +1,9 @@
-from functools import cmp_to_key
-
-def compare(x,y):
-    if len(x) > len(y):
-        return 1
-    elif len(x) < len(y):
-        return -1
-    else:
-        if x > y:
-            return 1
-        elif x < y:
-            return -1
-        else:
-            return 0
-
-
 N = int(input())
-list = []
+words = []
 
 for i in range(N):
-    list.append(input())
+    words.append(input())
 
-sort_list = sorted(set(list), key=cmp_to_key(compare))
-for str in sort_list:
-    print(str)
+words = sorted(set(words), key=lambda x: (len(x), x))
+for word in words:
+    print(word)
