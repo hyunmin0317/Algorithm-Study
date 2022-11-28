81246e9c81015673d00999bd9cee6afed9b8e8a3
hyunmin.0317 <hyunmin.0317@navercorp.com>
2022-11-28 14:22:55 +0900

    baekjoon: 11650

--- Python/baekjoon11650.py
+++ Python/baekjoon11650.py
@@ -1,23 +1,7 @@
-from functools import cmp_to_key
+N, points = int(input()), []
+for _ in range(N):
+    points.append(list(map(int, input().split())))
+points.sort()
 
-def compare(A,B):
-    if A[0] > B[0]:
-        return 1
-    elif A[0] < B[0]:
-        return -1
-    else:
-        if A[1] >= B[1]:
-            return 1
-        else:
-            return -1
-
-
-N = int(input())
-arr = []
-
-for i in range(N):
-    arr.append(list(map(int, input().split())))
-
-sort_list = sorted(arr, key=cmp_to_key(compare))
-for point in sort_list:
-    print(f'{point[0]} {point[1]}')
+for point in points:
+    print(point[0], point[1])
